de402151e17fb794a330e9f9192c76f7db0b8201
E'zoza Fayzullayeva <fayzullayevae22@gmail.com>
2023-09-26 23:59:04 +0500

    bajarildi ozgarishlar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,5 @@
 package org.example;
-
-import org.example.categories.Categories;
-import org.example.product.Product;
 import org.example.ui.Controller;
-import org.example.user.User;
-import org.example.user.UserType;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
-
 public class Main {
     public static void main(String[] args) {
 
